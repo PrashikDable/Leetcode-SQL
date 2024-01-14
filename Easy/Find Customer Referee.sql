7c7d6d1955a3d274f612acb03ee9111ad090b0f7
PrashikDable <prashikdable7@gmail.com>
2024-01-14 15:46:43 +0530

    2024-01-14

---
+++ Easy/Find Customer Referee.sql
@@ -0,0 +1,20 @@
+584. Find Customer Referee
+
+Table: Customer
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Find the names of the customer that are not referred by the customer with id = 2.
+Return the result table in any order.
+
+SOLUTION:
+
+SELECT NAME FROM CUSTOMER WHERE REFEREE_ID != 2 OR REFEREE_ID IS NULL;

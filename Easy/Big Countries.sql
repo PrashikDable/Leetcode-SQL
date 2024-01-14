74c2653153394d83156c781fdbf37184ae8fdca4
PrashikDable <prashikdable7@gmail.com>
2024-01-14 15:45:02 +0530

    2024-01-14

---
+++ Easy/Big Countries.sql
@@ -0,0 +1,27 @@
+595. Big Countries
+
+Table: World
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+name is the primary key (column with unique values) for this table.
+Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+ 
+
+A country is big if:
+it has an area of at least three million (i.e., 3000000 km2), or
+it has a population of at least twenty-five million (i.e., 25000000).
+Write a solution to find the name, population, and area of the big countries.
+Return the result table in any order.
+
+SOLUTION:
+
+SELECT NAME, POPULATION, AREA 
+FROM WORLD 
+WHERE POPULATION >= 25000000 OR AREA >= 3000000;

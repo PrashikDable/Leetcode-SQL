628760d634b2ae43d82843210eb949cb9f15d10b
PrashikDable <prashikdable7@gmail.com>
2024-01-16 14:06:28 +0530

    2024-01-16

---
+++ Easy/Article Views I.sql
@@ -0,0 +1,25 @@
+1148. Article Views I
+
+Table: Views
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+Return the result table sorted by id in ascending order.
+
+SOLUTION:
+
+SELECT DISTINCT AUTHOR_ID AS ID 
+FROM VIEWS 
+WHERE AUTHOR_ID = VIEWER_ID 
+ORDER BY ID
